31e8807462db085a2ff6b90de21167ac147f521d
Jakub Wadas <j.wadas1999@gmail.com>
2020-04-30 15:08:38 +0200

    Szkielety klas

---
+++ ProjektSSI/Core.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjektSSI
+{
+    public static class Core
+    {
+        //zlozenie wszystkich danych (predkosc i odczyt)
+        //Podjecie dzialania
+    }
+}
---
+++ ProjektSSI/ElementaryFunctions.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjektSSI
+{
+    public static class ElementaryFunctions
+    {
+        //Proporcja()
+        //Odczyt()
+        //Dzwonek()
+    }
+}
